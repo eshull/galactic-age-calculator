10207492ddc8cd4b2bdcec837378587faa0cff57
Eric Shull <eshulldesign@gmail.com>
2018-08-03 10:30:52 -0700

    add positive fail for now method to make new instance of time for the present time

--- spec/dob-spec.js
+++ spec/dob-spec.js
@@ -2,20 +2,25 @@ import {Dob} from '../src/dob';
 
 describe ('Dob', function() {
   let birthdate;
-  let sample;
+  let sampleBirthday;
+  let sampleNow;
 
 
   beforeEach(function() {
     birthdate = new Dob();
-    sample = new Date("September 13, 2000 00:00:00");
-
+    sampleBirthday = new Date("September 13, 2000 00:00:00");
+    sampleNow = new Date()
   });
 
 
   it ('should make a new birthday based on user input', function() {
-    // sample = "Date 2000-09-13T07:00:00.000Z";
     let dob = new Dob("September 13, 2000 00:00:00");
     console.log(dob);
-    expect(dob.birthday()).toEqual(sample);
+    expect(dob.birthday()).toEqual(sampleBirthday);
+  });
+  it ('should make a new instance of time', function() {
+    let presentDate = new Dob();
+    console.log(presentDate);
+    expect(presentDate.now()).toEqual(sample);
   });
 });
